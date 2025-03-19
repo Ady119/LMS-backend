2a6952f2b4d00e1a57d236784a2e787025ab446f
Adrian Leontescu <ady119@yahoo.com>
2025-03-19 16:55:01 +0000

    Fix port binding

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ app.register_blueprint(student_bp, url_prefix='/api/student')
 
 # Use Flask-Migrate Instead of `db.create_all()`
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'])
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
