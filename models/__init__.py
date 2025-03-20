d2fb6bae37b40ab219816d983b56917beb97bcea
Adrian Leontescu <ady119@yahoo.com>
2025-03-20 15:00:26 +0000

    Added manage.py for migrations

--- models/__init__.py
+++ models/__init__.py
@@ -9,10 +9,12 @@ from models.users import User
 from models.institutions import Institution
 from models.degrees import Degree 
 from models.enrolments import Enrolment
-from models.course_lecturers import CourseLecturer
 
+from models.course_lecturers import CourseLecturer
 from models.courses import Course
 from models.course_lessons import Lesson
+from models.lesson_section import LessonSection
+
 from models.quizzes import Quiz
 from models.short_quiz import ShortAnswerQuestion
 from models.multiple_choice import MultipleChoiceQuestion
@@ -20,8 +22,9 @@ from models.quiz_attempts import QuizAttempt
 from models.quiz_attempts_answers import QuizAttemptAnswer
 from models.quiz_results import QuizResult
 
-from models.lesson_section import LessonSection
 from models.assignment import Assignment
+from models.assignment_submission import AssignmentSubmission
+
 from models.exams import Exam
 
 
