e382156e3542f9dc81d9b0384151da8873d49730
Adrian Leontescu <ady119@yahoo.com>
2025-03-23 20:10:24 +0000

    Sync migrations with Heroku

--- config.py
+++ config.py
@@ -7,16 +7,22 @@ pymysql.install_as_MySQLdb()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'change_this_secret_key')      
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/lms_db2"
+    SQLALCHEMY_DATABASE_URI = None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "poolclass": QueuePool,
-        "pool_size": 5,
-        "max_overflow": 2,
-        "pool_timeout": 10
-    }
+    raw_db_url = os.getenv('DATABASE_URL')
     
+    if raw_db_url:
+        if raw_db_url.startswith("mysql://"):
+            # Replace "mysql://" with "mysql+pymysql://" 
+            raw_db_url = raw_db_url.replace("mysql://", "mysql+pymysql://", 1)
+        
+        parsed_url = urlparse(raw_db_url)
+        SQLALCHEMY_DATABASE_URI = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
+
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_URL', 'sqlite:///:memory:')
+
     # Dropbox API Token (Set in Heroku)
     DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
 
@@ -31,6 +37,14 @@ class Config:
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE", "Lax")
     PERMANENT_SESSION_LIFETIME = timedelta(days=1)
+     
+    # Other configurations...
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "poolclass": QueuePool,
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 10
+    }
 
 class DevConfig(Config):
     """Development Configuration"""
