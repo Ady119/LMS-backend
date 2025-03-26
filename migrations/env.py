700224248e6ce358f8997697e9779dea2e5e8a8e
Adrian Leontescu <ady119@yahoo.com>
2025-03-26 12:25:00 +0000

    Ensure migration file is pushed to Heroku

--- migrations/env.py
+++ migrations/env.py
@@ -37,6 +37,8 @@ def get_engine_url():
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
+target_db = current_app.extensions['migrate'].db
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
