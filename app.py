91260ad2af626d8495a16cf96f98af870ab2f7b5
Adrian Leontescu <ady119@yahoo.com>
2025-04-30 21:49:12 +0100

    Bring in StudentTakeQuiz.jsx from feature/offline-preview

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from flask import Flask
+
+from flask import Flask, send_file
 from flask_mail import Mail
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_session import Session
+
+from flask_socketio import SocketIO
+from flask_jwt_extended import JWTManager
+
 from config import config_dict
 from models import db
 from routes.authentication import auth_bp
 from routes.super_admin import admin_bp
 from routes.lecturers import lecturer_bp
 from routes.students import student_bp
+from routes.chat import chat_bp
 
-load_dotenv()
+# ----
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to the LMS App!"
-from flask import send_file
 
 @app.route('/loaderio-66e9ec91ed7e79e270da2de58f050f4a.txt')
 def loaderio_verification():
@@ -29,12 +34,13 @@ def loaderio_verification():
 env = os.environ.get("FLASK_ENV", "production")
 app.config.from_object(config_dict[env])
 print("Loaded DB URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
+
 CORS(app, resources={
     r"/*": {
         "origins": [
-            "https://lms-frontend-henna-sigma.vercel.app",                        # production
+            "https://lms-frontend-henna-sigma.vercel.app",
             "https://lms-frontend-5v355z5s0-adrians-projects-6add6cfa.vercel.app",
-            "https://lms-frontend-git-feature-offli-aed0ff-adrians-projects-6add6cfa.vercel.app", # preview
+            "https://lms-frontend-git-feature-offli-aed0ff-adrians-projects-6add6cfa.vercel.app",
         ],
         "supports_credentials": True
     }
@@ -45,13 +51,28 @@ db.init_app(app)
 mail = Mail(app)
 migrate = Migrate(app, db)
 
+socketio = SocketIO(
+    app,
+    async_mode="eventlet",
+    cors_allowed_origins="*",
+    cookie="access_token_cookie",
+    manage_session=False
+)
+
 print("Environment:", os.getenv("FLASK_ENV"))
 print("Database URI:", os.getenv("SQLALCHEMY_DATABASE_URI"))
 
-app.register_blueprint(auth_bp, url_prefix='/api/auth')
-app.register_blueprint(admin_bp, url_prefix='/api/admin')
-app.register_blueprint(lecturer_bp, url_prefix='/api/lecturer')
-app.register_blueprint(student_bp, url_prefix='/api/student')
+# register blueprints
+app.register_blueprint(auth_bp,      url_prefix='/api/auth')
+app.register_blueprint(admin_bp,     url_prefix='/api/admin')
+app.register_blueprint(lecturer_bp,  url_prefix='/api/lecturer')
+app.register_blueprint(student_bp,   url_prefix='/api/student')
+app.register_blueprint(chat_bp,      url_prefix='/api/chat')
 
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'])
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=app.config['DEBUG']
+    )
